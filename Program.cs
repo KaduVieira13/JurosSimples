96935595f1258cb18aed7521093478757acd83aa
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-21 14:04:38 -0300

    Juros simples pronto

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JurosSimples
+{
+              class Program
+              {
+                            static void Main(string[] args)
+                            {
+
+                                          decimal capital = 0; // valor
+                                          decimal tempo = 0; //Parcelas
+                                          decimal taxaDeJuros = 2; // Taxa de Juros
+                                          decimal juros;
+                                          decimal montante;
+
+                                          Console.Write("Digite o valor a ser investido: ");
+                                          capital = Convert.ToDecimal(Console.ReadLine());
+                                          Console.Write("Digite o numero de parcelas: ");
+                                          tempo = Convert.ToDecimal(Console.ReadLine());
+                                          juros = (capital / 100) * tempo * taxaDeJuros;
+                                          montante = capital + juros;
+                                          Console.WriteLine($"O valor do juros é: {juros:C2}");
+                                          Console.WriteLine($"O valor do montante é: {montante:C2}");
+
+
+
+                            }
+
+              }
+}
