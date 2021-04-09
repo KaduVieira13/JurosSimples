c025c908d5b488e964776162637e9caf91703d68
Diego Neri <diego@diegoneri.com>
2021-04-09 09:52:11 -0300

    Corrige fórmula e preenchimento da taxa de juros
    //o que deve ser divido por 100 é a taxa, pois, o usuário digita para 2%, o que equivale a 2 / 100 (dois por cento)

--- Program.cs
+++ Program.cs
@@ -1,30 +1,29 @@
-﻿using System;
+using System;
 
 namespace JurosSimples
 {
-              class Program
-              {
-                            static void Main(string[] args)
-                            {
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-                                          decimal capital = 0; // valor
-                                          decimal tempo = 0; //Parcelas
-                                          decimal taxaDeJuros = 2; // Taxa de Juros
-                                          decimal juros;
-                                          decimal montante;
+            decimal capital = 0; // valor
+            decimal tempo = 0; //Parcelas
+            decimal taxaDeJuros = 2; // Taxa de Juros
+            decimal juros;
+            decimal montante;
 
-                                          Console.Write("Digite o valor a ser investido: ");
-                                          capital = Convert.ToDecimal(Console.ReadLine());
-                                          Console.Write("Digite o numero de parcelas: ");
-                                          tempo = Convert.ToDecimal(Console.ReadLine());
-                                          juros = (capital / 100) * tempo * taxaDeJuros;
-                                          montante = capital + juros;
-                                          Console.WriteLine($"O valor do juros é: {juros:C2}");
-                                          Console.WriteLine($"O valor do montante é: {montante:C2}");
-
-
-
-                            }
-
-              }
+            Console.Write("Digite o valor a ser investido: ");
+            capital = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Digite o valor da taxa de juros: ");
+            taxaDeJuros = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Digite o numero de parcelas: ");
+            tempo = Convert.ToDecimal(Console.ReadLine());
+          
+            juros = capital * (taxaDeJuros / 100) * tempo ;
+            montante = capital + juros;
+            Console.WriteLine($"O valor do juros é: {juros:C2}");
+            Console.WriteLine($"O valor do montante é: {montante:C2}");
+        }
+    }
 }
